2df1326780332a6ddedaccc05eb39f55182dda1d
HenrikGabrielsson <Henrik.Gabben@gmail.com>
2014-02-28 09:31:00 +0100

    DALBase klar?

--- Aventyrliga-kontakter/Aventyrliga-kontakter/Model/DAL/DALBase.cs
+++ Aventyrliga-kontakter/Aventyrliga-kontakter/Model/DAL/DALBase.cs
@@ -1,11 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 
 namespace Aventyrliga_kontakter.Model.DAL
 {
     public abstract class DALBase
     {
+        //anslutning till databasen
+        private static string _connectionString;
+
+        //konstruktor
+        private static DALBase()
+        {
+            _connectionString = WebConfigurationManager.ConnectionStrings["ContactsConnectionString"].ConnectionString;
+        }
+
+        //Funktion som returnerar anslutningssträngen
+        protected SqlConnection CreateConnection()
+        {
+            SqlConnection conn = new SqlConnection(_connectionString);
+            return conn;
+        }
+
     }
 }
